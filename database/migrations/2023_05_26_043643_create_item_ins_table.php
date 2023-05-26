25e349e6eda01414bd0ab1bc486d2d6371640277
HennyMelinaPutri <201220073@unmuhpnk.ac.id>
2023-05-26 15:17:05 +0700

    memperbaharui database item_in

--- database/migrations/2023_05_26_043643_create_item_ins_table.php
+++ database/migrations/2023_05_26_043643_create_item_ins_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('item_ins', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->dateTime('tanggal_masuk');
         });
     }
 
