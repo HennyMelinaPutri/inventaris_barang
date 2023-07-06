6c8dd60242d385aff63a226895c8055370a9a975
JackHub5 <khairul.hermi@gmail.com>
2023-07-06 17:48:40 +0700

    memperbaharui tampilan kasar awal dan file login

---
+++ resources/views/layout/apk.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
+    <title>Inventaris Barang</title>
+</head>
+
+<body>
+    <header class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid p-3 ps-5">
+            <h3>INVENTARIS BARANG</h3>
+        </div>
+    </header>
+    <div>
+        @yield('konten')
+    </div>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
