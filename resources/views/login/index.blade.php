5261d2f79a8dac65a510774efb2e4a46684952dc
HennyMelinaPutri <201220073@unmuhpnk.ac.id>
2023-07-24 10:19:22 +0700

    memperbaharui tampilan login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,21 +1,46 @@
  @extends('layout/apk')
 
-@section('konten')
-    <div class="d-flex align-items-center py-4 bg-body-tertiary">
-        <div class="form-signin w-100 m-auto">
-            <form action="/login/admin" method="POST">
-                @csrf
-                <h1 class="h3 mb-3 fw-normal">Login</h1>
-                <div class="form">
-                    <label for="email" class="form-label">Email</label>
-                    <input name="email" type="email" value="{{Session::get('email')}}" class="form-control" id="floatingInput" placeholder="name@example.com">
-                </div>
-                <div class="form">
-                    <label for="password" class="form-label">Password</label>
-                    <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                </div>
-                <button name="submit" class="btn btn-primary w-100 py-2" type="submit">Login</button>
-            </form>
-        </div>
-    </div>
-@endsection
+ @section('konten')
+     <style>
+         .form-signin {
+             max-width: 330px;
+             padding: 1rem;
+         }
+
+         .form-signin .form-floating:focus-within {
+             z-index: 2;
+         }
+
+         .form-signin input[type="email"] {
+             margin-bottom: -1px;
+             border-bottom-right-radius: 0;
+             border-bottom-left-radius: 0;
+         }
+
+         .form-signin input[type="password"] {
+             margin-bottom: 10px;
+             border-top-left-radius: 0;
+             border-top-right-radius: 0;
+         }
+     </style>
+
+     <div>
+         <div class="form-signin w-100 m-auto">
+             <form action="/login/admin" method="POST">
+                 @csrf
+                 <h1 class="h3 mb-3 fw-bold">Login</h1>
+                 <div class="form pt-2">
+                     <label for="email" class="form-label fw-medium">Email</label>
+                     <input name="email" type="email" value="{{ Session::get('email') }}" class="form-control"
+                         id="floatingInput" placeholder="name@example.com">
+                 </div>
+                 <div class="form pt-3">
+                     <label for="password" class="form-label fw-medium">Password</label>
+                     <input name="password" type="password" class="form-control" id="floatingPassword"
+                         placeholder="Password">
+                 </div>
+                 <button name="submit" class="btn btn-primary w-100 py-2 mt-4 fw-bold" type="submit">Login</button>
+             </form>
+         </div>
+     </div>
+ @endsection
