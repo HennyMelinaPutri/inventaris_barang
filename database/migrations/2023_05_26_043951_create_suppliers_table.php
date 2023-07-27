d6200f5c2be0b842510a61c2b5de43380ec6b7e1
JackHub5 <khairul.hermi@gmail.com>
2023-07-27 14:23:34 +0700

    memperbaharui table supplier

--- database/migrations/2023_05_26_043951_create_suppliers_table.php
+++ database/migrations/2023_05_26_043951_create_suppliers_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('suppliers', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_supplier');
+            $table->string('nama_supplier');
+            $table->string('alamat');
+            $table->integer('telepon');
             $table->timestamps();
         });
     }
