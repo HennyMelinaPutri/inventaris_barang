45b4cc4c6b71ec839ecf3df70723ea005e926930
AdeFirmansyah97 <201220001@unmuhpnk.ac.id>
2023-07-27 14:16:02 +0700

    edit controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     function index()
     {
-        return view('login/index');
+        return view('login.index');
     }
 
     function login(Request $request)
